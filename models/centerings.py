f12423dcee923fb6af575f5ccc6516be221ab213
Marcel Gauglitz <marcel_gauglitz@yahoo.com>
2025-09-08 15:28:36 +0200

    Sinkhorn Knopp Centering implementation

--- models/centerings.py
+++ models/centerings.py
@@ -1,26 +1,22 @@
 import torch
 import torch.nn as nn
+
 # TODO: maybe it is better to implement a abstract class and to inherit from it
 
 
 class EMAPatchCentering(nn.Module):
 
     def __init__(
-            self,
-            hidden_dim: int,
-            center_momentum: float = 0.9,
+        self,
+        hidden_dim: int,
+        center_momentum: float = 0.9,
     ) -> None:
         super().__init__()
         self.center_momentum = center_momentum
-        self.center = nn.Parameter(
-            torch.zeros([1, 1, hidden_dim])
-        )
+        self.center = nn.Parameter(torch.zeros([1, 1, hidden_dim]))
 
     @torch.no_grad()
-    def forward(
-            self,
-            x: torch.Tensor
-    ) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         assert x.ndim == self.center_momentum.ndim, (
             "Sorry, please ensure that the number of dimensions between the "
             "input tensor and the output centering tensor is the same."
@@ -28,10 +24,7 @@ class EMAPatchCentering(nn.Module):
         return x - self.center
 
     @torch.no_grad()
-    def update_center(
-            self,
-            x: torch.Tensor
-    ) -> None:
+    def update_center(self, x: torch.Tensor) -> None:
         self.center = self.center * self.center_momentum + x * (
             1 - self.center_momentum
         )
@@ -40,21 +33,16 @@ class EMAPatchCentering(nn.Module):
 class EMAClassCentering(nn.Module):
 
     def __init__(
-            self,
-            hidden_dim: int,
-            center_momentum: float = 0.9,
+        self,
+        hidden_dim: int,
+        center_momentum: float = 0.9,
     ) -> None:
         super().__init__()
         self.center_momentum = center_momentum
-        self.center = nn.Parameter(
-            torch.zeros([1, hidden_dim])
-        )
+        self.center = nn.Parameter(torch.zeros([1, hidden_dim]))
 
     @torch.no_grad()
-    def forward(
-            self,
-            x: torch.Tensor
-    ) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         assert x.ndim == self.center_momentum.ndim, (
             "Sorry, please ensure that the number of dimensions between the "
             "input tensor and the output centering tensor is the same."
@@ -62,10 +50,80 @@ class EMAClassCentering(nn.Module):
         return x - self.center
 
     @torch.no_grad()
-    def update_center(
-            self,
-            x: torch.Tensor
-    ) -> None:
+    def update_center(self, x: torch.Tensor) -> None:
         self.center = self.center * self.center_momentum + x * (
             1 - self.center_momentum
         )
+
+
+class SinkhornKnopp(nn.Module):
+
+    def __init__(
+        self, teacher_temp: float = 0.2, n_iterations: int = 3
+    ) -> None:
+        super().__init__()
+        self.n_iterations = n_iterations
+
+    def forward(
+        self,
+        teacher_token: torch.Tensor,
+        teacher_temp: float,
+        mask: torch.Tensor | None = None,
+    ):
+        teacher_token = teacher_token.float()
+
+        if teacher_token.ndim == 3:
+            assert mask is not None, (
+                "You need to specify a mask pattern if you want to use "
+                "Sinkhorn Knopp on patches."
+            )
+            teacher_token = teacher_token.flatten(0, 1)
+            hidden_dim = teacher_token.shape[-1]
+            batch_num = torch.sum(mask)
+            teacher_token = torch.exp(
+                teacher_token / teacher_temp
+            ) * mask.float().unsqueeze(-1)
+
+        else:
+            hidden_dim = teacher_token.shape[-1]
+            batch_num = teacher_token.shape[0]
+            teacher_token = torch.exp(teacher_token / teacher_temp)
+
+        teacher_token = teacher_token.t()
+        teacher_token = teacher_token / torch.sum(teacher_token)
+        for i in range(self.n_iterations):
+            batch_sum = torch.sum(teacher_token, dim=1, keepdim=True).clamp(
+                1e-16
+            )
+
+            teacher_token = teacher_token / batch_sum
+
+            teacher_token = teacher_token / hidden_dim
+
+            dim_sum = torch.sum(teacher_token, dim=0, keepdim=True).clamp(
+                1e-16
+            )
+
+            teacher_token = teacher_token / dim_sum
+
+            teacher_token = teacher_token / batch_num
+
+        teacher_token = teacher_token * batch_num
+        return teacher_token.t()
+
+
+if __name__ == "__main__":
+    BATCH_SIZE = 2
+    SEQ_LENGTH = 3
+    HIDDEN_DIM = 4
+
+    teacher_out_patches = torch.randn([BATCH_SIZE, SEQ_LENGTH, HIDDEN_DIM])
+
+    mask = (torch.randn([BATCH_SIZE * SEQ_LENGTH]) < 0.5).bool()
+
+    sink_horn = SinkhornKnopp()
+    result = sink_horn(
+        teacher_token=teacher_out_patches, teacher_temp=0.5, mask=mask
+    )
+
+    print(result)
